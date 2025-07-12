f551ff86d5cf3eee35d2026fef95e4274be00c95
Deep Gadhiya <Deepvgadhiya@gmail.com>
2025-07-12 11:50:53 +0530

    answer's model

--- backend/src/models/Answer.model.js
+++ backend/src/models/Answer.model.js
@@ -4,7 +4,9 @@ const answerSchema = new mongoose.Schema({
   question: { type: mongoose.Schema.Types.ObjectId, ref: 'Question', required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   description: { type: String, required: true },
-  isAccepted: { type: Boolean, default: false }
+  upvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  downvotes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  isPinned: { type: Boolean, default: false }
 }, { timestamps: true });
 
 export default mongoose.model('Answer', answerSchema);
